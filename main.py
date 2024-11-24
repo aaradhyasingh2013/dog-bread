3024e0056f3be014785450e23db7f769ce9c46b9
aaradhyasingh2013 <kuki81kumari@gmail.com>
2024-11-24 20:24:41 +0530

    dog  bred

---
+++ main.py
@@ -0,0 +1,8 @@
+  init (self, max_speed, mileage):
+  self.max_speed = max_speed
+  self.mileage = mileage
+  modelx = Vehicle(240, 18)
+  print("Model Max Speed:",modelX.max_speed)
+  print("Model Mileage:", modelX.mileage)
+  class Vehicle:
+def init (self, max_speed, mileage):
